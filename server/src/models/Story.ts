392110d055cd144886a277dc996608b60f9bcad9
Atharva Wankhede <atharvamilind123@gmail.com>
2023-07-11 20:04:17 +0530

    Added Story model

---
+++ server/src/models/Story.ts
@@ -0,0 +1,28 @@
+import { Schema, Types, model } from "mongoose";
+
+interface StoryType {
+  image: string;
+  caption?: string;
+  author: Types.ObjectId;
+}
+
+const storySchema: Schema = new Schema<StoryType>(
+  {
+    image: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    caption: Schema.Types.String,
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Story = model<StoryType>("Story", storySchema);
+
+export default Story;
