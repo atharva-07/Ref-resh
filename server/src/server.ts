430a3127113ee77c6908db56e4583cbd82bc1f0c
Atharva Wankhede <atharvamilind123@gmail.com>
2023-07-12 02:24:20 +0530

    MongoDb Connection Setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
-import * as dotenv from 'dotenv';
-import 'reflect-metadata';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import { typeDefs, resolvers } from './graphql/schema';
+import * as dotenv from "dotenv";
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { typeDefs, resolvers } from "./graphql/schema";
+import mongoose from "mongoose";
 
 interface MyContext {
   token?: string;
@@ -15,28 +16,37 @@ interface MyContext {
 
 dotenv.config();
 
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URI}/${process.env.MONGODB_DBNAME}?retryWrites=true`;
+
 const app = express();
 const httpServer = http.createServer(app);
 
 const server = new ApolloServer<MyContext>({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 await server.start();
 
 app.use(
-  '/',
+  "/",
   cors<cors.CorsRequest>(),
   bodyParser.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token })
+    context: async ({ req }) => ({ token: req.headers.token }),
   })
 );
 
-new Promise<void>((resolve) =>
-  httpServer.listen({ port: process.env.DEV_PORT || 4000 }, resolve)
-).then(() => {
-  console.log(`Server is up and running at ${process.env.DEV_PORT}`);
-});
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Database Connected.");
+    httpServer.listen({ port: process.env.DEV_PORT || 4000 });
+  })
+  .then(() => {
+    console.log(`Server is up and running at ${process.env.DEV_PORT}`);
+  })
+  .catch((err) => {
+    console.log("Error establishing the connection.", err);
+  });
