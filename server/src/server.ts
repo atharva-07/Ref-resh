34f0005633a814faf5f9cbc7e7b624707ffe1952
Atharva Wankhede <atharvamilind123@gmail.com>
2023-07-17 23:29:33 +0530

    formatError() :)

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import "reflect-metadata";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerErrorCode } from "@apollo/server/errors";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
 import http from "http";
@@ -9,6 +10,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { typeDefs, resolvers } from "./graphql/schema";
 import mongoose from "mongoose";
+import { HttpErrorResponse } from "./graphql/utility-types";
 
 interface AppContext {
   token?: string;
@@ -26,15 +28,51 @@ const server = new ApolloServer<AppContext>({
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   formatError(formattedError, error) {
-    console.log(formattedError);
-    return formattedError;
+    const errorResponse: HttpErrorResponse = {
+      success: false,
+      code: formattedError.extensions?.code || 500,
+      message: formattedError.message,
+    };
+    if (
+      formattedError.extensions?.code === ApolloServerErrorCode.BAD_USER_INPUT
+    ) {
+      errorResponse.code = 400;
+      errorResponse.message = "Invalid value passed for a field.";
+    }
+    if (formattedError.extensions?.code === ApolloServerErrorCode.BAD_REQUEST) {
+      errorResponse.code = 400;
+      errorResponse.message = "An unexpected error occured before parsing.";
+    }
+    if (
+      formattedError.extensions?.code ===
+      ApolloServerErrorCode.GRAPHQL_VALIDATION_FAILED
+    ) {
+      errorResponse.code = 400;
+      errorResponse.message =
+        "Query doesn't match the schema. Try double-checking it.";
+    }
+    if (
+      formattedError.extensions?.code ===
+      ApolloServerErrorCode.GRAPHQL_PARSE_FAILED
+    ) {
+      errorResponse.code = 400;
+      errorResponse.message =
+        "Operation could not be completed due to incorrect syntax.";
+    }
+    if (
+      formattedError.extensions?.code ===
+      ApolloServerErrorCode.INTERNAL_SERVER_ERROR
+    ) {
+      errorResponse.code = 500;
+    }
+    return errorResponse;
   },
 });
 
 await server.start();
 
 app.use(
-  "/",
+  "/api",
   cors<cors.CorsRequest>(),
   bodyParser.json(),
   expressMiddleware(server, {
