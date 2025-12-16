b2035a7004873b0756d16603ff27a68bdb2f17dc
Atharva Wankhede <atharvamilind123@gmail.com>
2025-12-16 21:09:10 +0530

    Small changes to shadcn/ui components

--- client/src/components/ui/calendar-13.tsx
+++ client/src/components/ui/calendar-13.tsx
@@ -1,14 +1,6 @@
 import * as React from "react";
 
 import { Calendar } from "@/components/ui/calendar";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 export default function Calendar13({
   selected,
